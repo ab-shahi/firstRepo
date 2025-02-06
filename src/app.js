465827a5bb08759d8a8a41c47b4040a11c74122d
ab-shahi <ab.kumar7001@gmail.com>
2025-02-07 04:05:37 +0530

    initial commit

--- src/app.js
+++ src/app.js
@@ -3,6 +3,15 @@ const express = require('express')
 const app = express()
 const port = 7777
 
+app.post("/homepage", (req,res) => {
+    res.send("This is homepage post req!!!")
+})
+
+app.get("/homepage", (req,res) => {
+    res.send("This is homepage get req!!!")
+})
+
+
 app.use("/homepage", (req,res) => {
     res.send("This is homepage!!!")
 })
